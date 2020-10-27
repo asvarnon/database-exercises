da3799b0808ab829911d212ec30c3c81c51273f1
Austin Varnon <austinvarnon@gmail.com>
2020-10-27 13:39:21 -0500

    finished exercises revised

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,5 +1,5 @@
 USE codeup_test_db;
 
-SELECT name, release_date FROM albums WHERE release_date > 1991;
-SELECT name, genre FROM albums WHERE genre = 'disco';
-SELECT name, artist FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE artist = 'Michael Jackson';
