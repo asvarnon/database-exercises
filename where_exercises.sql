53bc51b7cb8b9b6c60f3d1cce8ba9c4997536167
Austin Varnon <austinvarnon@gmail.com>
2020-10-28 11:14:10 -0500

    finishing the WHERE module

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,5 @@
 USE employees;
 
-SELECT * FROM employees where first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT hire_date, first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
