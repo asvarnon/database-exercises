6b3dc4df30fbf96f185515d060ede51d25f31b33
Austin Varnon <austinvarnon@gmail.com>
2020-10-29 15:43:58 -0500

    FEAT: done

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,14 +9,22 @@ where dm.to_date = '9999-01-01';
 
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees AS e
-    JOIN dept_manager dm ON e.emp_no = dm.emp_no
-    JOIN departments d ON dm.dept_no = d.dept_no
+    JOIN dept_manager as dm ON e.emp_no = dm.emp_no
+    JOIN departments as d ON dm.dept_no = d.dept_no
 WHERE to_date = '9999-01-01';
 
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
 FROM employees AS e
-     JOIN dept_manager dm ON e.emp_no = dm.emp_no
-     JOIN departments d ON dm.dept_no = d.dept_no
+     JOIN dept_manager as dm ON e.emp_no = dm.emp_no
+     JOIN departments as d ON dm.dept_no = d.dept_no
 WHERE to_date = '9999-01-01' AND e.gender = 'f';
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+FROM employees AS e
+         JOIN dept_manager as dm ON e.emp_no = dm.emp_no
+         JOIN departments as d ON dm.dept_no = d.dept_no
+         JOIN salaries as s on e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+ORDER BY salary;
 
 
