4a6dff7e497f80171125f2837b8213f045ebe1e9
Austin Varnon <austinvarnon@gmail.com>
2020-10-30 14:02:51 -0500

    Group-by exercises #1 completed

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -14,7 +14,19 @@ SELECT hire_date, COUNT(*)
 FROM employees
 GROUP BY hire_date
 ORDER BY COUNT(*)
-limit 12;
+
+# Exercises
+
+SELECT DISTINCT dept_name, COUNT(*)
+FROM departments
+GROUP BY dept_name
+ORDER BY COUNT(*) ASC
+Limit 20;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%e';
+
 
 
 
