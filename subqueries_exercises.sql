d02b6cf801f5fbacd03169c78d761db48c23893f
Austin Varnon <austinvarnon@gmail.com>
2020-10-30 11:05:14 -0500

    Finished subqueries exercisess

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,1 +1,25 @@
 USE employees;
+
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name
+FROM employees
+    WHERE hire_date = (SELECT hire_date
+                        FROM employees
+                        WHERE emp_no = 101010);
+
+
+SELECT title, emp_no
+FROM titles
+WHERE emp_no IN (SELECT emp_no
+                 FROM employees
+                 WHERE first_name = 'Aamod');
+
+
+# Find all the current department managers that are female.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name
+FROM employees
+WHERE emp_no IN (SELECT emp_no
+                FROM dept_manager
+                WHERE to_date = '9999-01-01')
+AND gender = 'F';
+
+
